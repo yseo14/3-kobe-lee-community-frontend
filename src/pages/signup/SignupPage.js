7f9eb14ede585a31855a3fd0dd65628e4d33d63a
yseo14 <lmslys14@naver.com>
2025-10-20 22:36:38 +0900

    feat: 회원가입 API 연동

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -2,6 +2,7 @@ import InputField from "../../components/input-field/InputField.js";
 import ProfileUpload from "../../components/profile-upload/ProfileUpload.js";
 import Button from "../../components/button/Button.js";
 import { navigate } from "../../main.js";
+import { BASE_URL } from "../../config/api.js";
 
 export default function SignupPage() {
   const container = document.createElement("div");
@@ -60,13 +61,69 @@ export default function SignupPage() {
     text: "회원가입",
     className: "primary",
     width: "320px",
-    onClick: (e) => {
+    onClick: async (e) => {
       e.preventDefault();
-      console.log("회원가입 시도");
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+      const confirmPassword = document
+        .getElementById("passwordConfirm")
+        .value.trim();
+      const nickname = document.getElementById("nickname").value.trim();
+
+      const message =
+        document.querySelector(".message") || document.createElement("p");
+      message.className = "message";
+      message.style.marginTop = "12px";
+
+      if (!container.contains(message)) container.appendChild(message);
+      if (!email || !password || !confirmPassword || !nickname) {
+        message.textContent = "모든 필드를 입력하세요.";
+        message.style.color = "red";
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        message.textContent = "비밀번호가 일치하지 않습니다.";
+        message.style.color = "red";
+        return;
+      }
+
+      try {
+        const res = await fetch(`${BASE_URL}/member`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            nickname,
+            password,
+            confirmPassword,
+            imageId: null,
+          }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok || !data.success) {
+          message.textContent = `${data.message || "회원가입 실패"}`;
+          message.style.color = "red";
+          return;
+        }
+
+        message.textContent = "회원가입 성공! 로그인 페이지로 이동합니다.";
+        message.style.color = "green";
+
+        // 1초 후 로그인 페이지로 이동
+        setTimeout(() => navigate("/login"), 1000);
+      } catch (err) {
+        console.error("회원가입 요청 실패:", err);
+        message.textContent = "서버와 연결할 수 없습니다";
+        message.style.color = "red";
+      }
     },
   });
   container.appendChild(signupButton.render());
-
 
   const goLogin = new Button({
     text: "로그인하러 가기",
