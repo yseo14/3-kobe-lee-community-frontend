d4ee9c696ef2b1e4435d7e9395bec3e9ef6c6659
yseo14 <lmslys14@naver.com>
2025-10-24 22:55:26 +0900

    feat: Footer 컴포넌트 추가 및 이용약관, 개인정보처리방침 추가 구현

---
+++ src/api/policyApi.js
@@ -0,0 +1,15 @@
+import { apiRequest } from "./api.js";
+
+// 이용약관
+export function getTerms() {
+  return apiRequest("/terms", {
+    method: "GET",
+  });
+}
+
+// 개인정보처리방침
+export function getPrivacy() {
+  return apiRequest("/privacy", {
+    method: "GET",
+  });
+}
