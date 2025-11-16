6e05a2c7dda3d714d2b3247a6cf322f2eddc2345
yseo14 <lmslys14@naver.com>
2025-11-16 17:42:15 +0900

    feat: alb 도메인으로 수정

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080" // 개발 중에 로컬 서버로 연결
-    : "http://13.125.4.68:8080"; // 배포시 배포 서버로 연결
+    : "https://community-alb-1133986083.ap-northeast-2.elb.amazonaws.com"; // 배포시 배포 서버로 연결
